023fe243bb6277efe277a83b77ea5ce642e02fc5
Aleksandr Vakhitov <ale.vakhitov@gmail.com>
2022-05-05 10:27:40 +0500

    0

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,8 @@
 
-// Tabs
-class Tabs {
-  constructor(headerSelector, tabSelector, contentSelector, activeClass) {
-    this.headerSelector = headerSelector;
-    this.tabSelector = tabSelector;
-    this.contentSelector = contentSelector;
-    this.activeClass = activeClass;
-    const header = document.querySelector(headerSelector);
-    const tab = document.querySelectorAll(tabSelector);
-    const content = document.querySelectorAll(contentSelector);
-
-    function hideTabContent() {
-      content.forEach(item => {
-        item.style.display = 'none';
-      });
-      tab.forEach(item => {
-        item.classList.remove(activeClass);
-      });
-    }
-
-    function showTabContent(i = 0) {
-      content[i].style.display = 'block';
-      tab[i].classList.add(activeClass);
-    }
-
-    hideTabContent();
-    showTabContent();
-
-    header.addEventListener('click', (e) => {
-      e.preventDefault();
-      if (e.target && (e.target.classList.contains(tabSelector.replace(/\./, "")) ||
-        e.target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
-        tab.forEach((item, i) => {
-          if (e.target == item || e.target.parentNode == item) {
-            hideTabContent();
-            showTabContent(i);
-          }
-        });
-      }
-    });
-  }
-}
-
 window.addEventListener("DOMContentLoaded", () => {
 
   let viewMode = document.querySelectorAll('.view-mode__link');
   let cardProductGrid = document.querySelectorAll('.card-product-grid');
-
   viewMode.forEach(item => {
     item.addEventListener('click', function () {
       viewMode.forEach(item => {
@@ -60,8 +16,6 @@ window.addEventListener("DOMContentLoaded", () => {
       })
     });
   })
-
-
 
   // Функция берет обьект по классу и обрезает его текст по условию
   let cropText = function () {
